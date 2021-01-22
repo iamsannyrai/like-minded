7087ec8efe1f2e6932d77a235dd77acf02150c4c
@iamsannyrai <iam.sannyrai@gmail.com>
2021-01-22 22:53:05 +0545

    designs registration page

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,217 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:like_minded/utils/color_utils.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Like Minded',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: AccountCreationPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class AccountCreationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: ListView(
+          children: [
+            SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/images/logo.png', height: 50),
+                SizedBox(width: 6),
+                SvgPicture.asset('assets/svgs/logo-text.svg', height: 40),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            SizedBox(height: 40),
+            Align(
+              alignment: Alignment.center,
+              child: RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'Create your',
+                      style: TextStyle(fontSize: 25, color: primaryMain),
+                    ),
+                    TextSpan(
+                      text: '\nConnection',
+                      style: TextStyle(
+                          color: primaryDark,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            // form fields
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Name'),
+                  SizedBox(height: 6),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 0,
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide: const BorderSide(color: primaryDark),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(6)),
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                        borderSide: const BorderSide(color: primaryMain),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(6)),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 14),
+                  // email
+                  Text('Email'),
+                  SizedBox(height: 6),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 0,
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide: const BorderSide(color: primaryDark),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(6)),
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                        borderSide: const BorderSide(color: primaryMain),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(6)),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 14),
+                  // password
+                  Text('Password'),
+                  SizedBox(height: 6),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 0,
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide: const BorderSide(color: primaryDark),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(6)),
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                        borderSide: const BorderSide(color: primaryMain),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(6)),
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.remove_red_eye),
+                        color: Color(0xffDFDEFD),
+                        onPressed: () {
+                          print('hide');
+                        },
+                      ),
+                    ),
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 16),
+                  // privacy policy
+                  RichText(
+                    text: TextSpan(
+                      text: 'By clicking below, you agree to our ',
+                      style: TextStyle(color: primaryDark),
+                      children: [
+                        TextSpan(
+                          text: 'Terms of Service',
+                          style: TextStyle(
+                              color: primaryMain, fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(
+                          text: ' and ',
+                          style: TextStyle(color: primaryDark),
+                        ),
+                        TextSpan(
+                          text: 'Privacy Policy.',
+                          style: TextStyle(
+                              color: primaryMain, fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  // register button
+                  Row(
+                    children: [
+                      Expanded(
+                        child: RaisedButton(
+                          color: primaryMain,
+                          child: Text(
+                            'Create Account',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  // already connected
+                  GestureDetector(
+                    child: RichText(
+                      text: TextSpan(
+                        text: 'Already connected? ',
+                        style: TextStyle(color: primaryDark),
+                        children: [
+                          TextSpan(
+                            text: 'Login',
+                            style: TextStyle(
+                              color: primaryMain,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    onTap: () {
+                      //TODO
+                      print('login');
+                    },
+                  )
+                ],
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
---
+++ lib/utils/color_utils.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const primaryDark = Color(0xff343090);
+const primaryMain = Color(0xff5F59F7);
+    
